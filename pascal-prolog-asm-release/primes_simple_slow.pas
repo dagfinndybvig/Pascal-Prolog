2c6a38eb0d2806c22cc5e03825d27285d209053a
dagfinndybvig <dagfinn.dybvig@gmail.com>
2026-04-15 15:11:31 +0200

    Add working simple comparison programs
    - primes_simple_slow.pas: Working slow version (naive subtraction)
    - primes_simple_fast.pas: Working fast version (square root optimized)
    - SIMPLE_COMPARISON.md: Documentation for the simple comparison
    - Both correctly find 168 primes under 1000
    - Demonstrates algorithmic differences with working code
    - Provides foundation for understanding performance impact

---
+++ pascal-prolog-asm-release/primes_simple_slow.pas
@@ -0,0 +1,35 @@
+program primes_simple_slow;
+
+var
+  i, j, is_prime, temp, is_divisible, count: integer;
+
+begin
+  i := 2;
+  count := 0;
+  
+  while i <= 1000 do
+  begin
+    is_prime := 1;
+    j := 2;
+    while j < i do
+    begin
+      is_divisible := 0;
+      temp := i;
+      while temp >= j do
+      begin
+        temp := temp - j;
+        if temp = 0 then is_divisible := 1;
+      end;
+      if is_divisible = 1 then
+      begin
+        is_prime := 0;
+        j := i;
+      end;
+      j := j + 1;
+    end;
+    if is_prime = 1 then count := count + 1;
+    i := i + 1;
+  end;
+  
+  writeln(count);
+end.
